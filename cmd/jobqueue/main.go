e00caea6105b31b90c78c3ff4a31530991955076
yoichi-aoyama <aoyama.yoichi@gmail.com>
2023-02-06 00:59:47 +0900

    add: jobqueue, docker, hello

---
+++ cmd/jobqueue/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"github.com/olivere/jobqueue"
+	"github.com/olivere/jobqueue/mysql"
+	"time"
+)
+
+func main() {
+
+	store, err := mysql.NewStore("root@tcp(127.0.0.1:3306)/jobqueue_e2e?loc=UTC&parseTime=true")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a manager with the MySQL store and 10 concurrent workers.
+	m := jobqueue.New(
+		jobqueue.SetStore(store),
+		//jobqueue.SetConcurrency(10),
+	)
+
+	// Register one or more topics and their processor
+	//m.Register("clicks", func(args ...interface{}) error {
+	//	// Handle "clicks" topic
+	//})
+
+	// Start the manager
+	err = m.Start()
+	if err != nil {
+		panic(err)
+	}
+
+	// Add a job: It'll be added to the store and processed eventually.
+	err = m.Add(&jobqueue.Add{Topic: "clicks", Args: []interface{}{640, 480}})
+	if err != nil {
+		panic(err)
+	}
+
+	// Stop the manager, either via Stop/Close (which stops after all workers
+	// are finished) or CloseWithTimeout (which gracefully waits for a specified
+	// time span)
+	err = m.CloseWithTimeout(15 * time.Second) // wait for 15 seconds before forced stop
+	if err != nil {
+		panic(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module yoichi-aoyama/go-sample
 
 go 1.19
+
+require (
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/olivere/jobqueue v1.3.1 // indirect
+)
